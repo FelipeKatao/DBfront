158bf94d28acf84eced7edd2578a0cc7f8503dc7
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-11-11 01:57:34 -0300

    update

--- dbfront/src/App.js
+++ dbfront/src/App.js
@@ -1,16 +1,16 @@
 import NavComp from './components/NavComp.js'
 import TableComp from './components/TableComp.js'
-
+import {BrowserRouter, Switch, Route}  from 'react-router-dom';
 
 function App() {
   
   return (
     <>
     <NavComp></NavComp>
-    <div className="App">
-      </div>
-      <TableComp></TableComp>
-      </>
+    <Switch>
+      <Route path="/funcionarios" component={TableComp}></Route>
+    </Switch>
+    </>
   )
 }
 
--- dbfront/src/components/NavComp.js
+++ dbfront/src/components/NavComp.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 import '../css/components.css'
+
 
 function navComp(){
     return (
         <nav>
             <ul>
                 <li><h3>Banco de dados de funcionarios</h3></li>
+                <li><Link to='/funcionarios'>Listar</Link></li>
                 <li>adicionar</li>
                 <li>Remover</li>
                 <li>Procurar</li>
